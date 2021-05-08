dfbc84c79a820b9da835e4739f3340a6de66a757
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-08 11:10:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export default {
-  tgUsername: "TheShashank",
-  githubUsername: "WhiteDevil-OP",
+  tgUsername: "xD_Shaahank",
+  githubUsername: "Theshashankk",
   websiteName: "WhiteDevilX",
   heroHead: "Hlo, Bruh!",
   heroText: "Hey I'm shashank Form jharkhand",
   facebookUsername: "TheshashankSharma",
   instaUsername: "Real_Sharmashashank",
-  typedArray : ["A Noob Developer 👨🏻‍💻","Slow Learner 🤣","Song Lover 🎶"," FF Player 🤓"]
+  typedArray : ["A Noob Developer 👨🏻‍💻","Slow Learner 🤣","Song Lover 🎶"," FF Player 🤓", "Always using TG😅"]
 };
