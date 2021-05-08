c3bc0b6a7fbb9c36309fb26e358da2f7f94e4892
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-08 11:27:44 +0530

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -15,7 +15,7 @@ export default function Customhead() {
               width="30"
               height="30"
               className="d-inline-block align-top"
-              alt="aryanvikash"
+              alt="TheShashank"
             />
             <kbd id="username">{config.websiteName}</kbd>
           </a>
